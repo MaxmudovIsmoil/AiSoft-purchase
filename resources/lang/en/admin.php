ea7561ded2f10d2bc067ad714c87a79ae4713b12
Azizbek <ai1996yil@gmail.com>
2024-01-27 12:09:30 +0500

    Feature Task 2 lang

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+// lang/en/messages.php
+
+return [
+    'Administrator' => 'Administrator',
+    'Users' => 'Users',
+    'Roles' => 'Roles',
+    'Permission' => 'Permission',
+    'Category' => 'Category',
+];
---
+++ resources/lang/ru/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+// lang/en/messages.php
+
+return [
+    'Administrator' => 'Administrator',
+    'Users' => 'Users',
+    'Roles' => 'Roles',
+    'Permission' => 'Permission',
+];
